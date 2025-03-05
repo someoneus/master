62ddaa0c550e01c464651268559b8277fb21a792
someoneus <wilsonjunor7@gmail.com>
2025-03-05 20:37:05 +0700

    Update someoneussource.lua

--- someoneussource.lua
+++ someoneussource.lua
@@ -1179,7 +1179,39 @@ local Button = Tab:CreateButton({
 
 
 local Tab = Window:CreateTab("Hub's", 4483362458) -- Title, Image
-local Section = Tab:CreateSection("FE R6 Animation",false) -- The 2nd argument is to tell if its only a Title and doesnt contain element
+local Section = Tab:CreateSection("FE",false) -- The 2nd argument is to tell if its only a Title and doesnt contain element
+
+local Button = Tab:CreateButton({
+   Name = "FE Satan",
+   Interact = 'Click',
+   Callback = function()
+   -- The function that takes place when the button is pressed
+   --[[
+fe satan converted by maxilito
+
+https://www.roblox.com/catalog/10775031176/The-Pogo
+https://www.roblox.com/catalog/12103236257/AO-Surfboard
+https://www.roblox.com/catalog/9867481700/Le-Rouge-Cross-body-Bag-1-0
+https://www.roblox.com/catalog/9867487176/Le-Rouge-Cross-body-Bag-3-0
+https://www.roblox.com/catalog/9350274205/Vans-Black-White-Checkerboard-Umbrella
+https://www.roblox.com/catalog/2493590711/Knights-of-Redcliff-Paladin-Helmet
+https://www.roblox.com/catalog/9402600266/Sketched-white-and-magenta-5G-wings
+https://www.roblox.com/catalog/2309346267/Classic-PC-Hat
+
+-gh 10775031176, 12103236257, 9867481700, 9867487176, 9350274205, 2493590711, 9350274205, 9402600266, 2309346267
+--]]
+
+--this code below will give you the required hats instantly so you doesn't need to wear any hats
+
+if not game:GetService("ReplicatedStorage"):FindFirstChild("01_server") then return game:GetService("TeleportService"):Teleport(17574618959, game:GetService("Players").LocalPlayer) end
+game.Players:Chat("-gh 10775031176, 12103236257, 9867481700, 9867487176, 9350274205, 2493590711, 9350274205, 9402600266, 2309346267")
+--just a baseplate only
+wait(2.1)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty36.lua"))()
+   end,
+})
+
 local Button = Tab:CreateButton({
    Name = "HatHub (Patched)",
    Interact = 'Click',
@@ -1385,5 +1417,14 @@ local Button = Tab:CreateButton({
    Callback = function()
    -- The function that takes place when the button is pressed
    loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Roblox-Anticheat-finder-30874"))()
+
+local Button = Tab:CreateButton({
+   Name = "Aim-ware (aimbots)",
+   Interact = 'Click',
+   Callback = function()
+   -- The function that takes place when the button is pressed
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneus/master/refs/heads/main/aimwaresource.lua"))()
+   end,
+})
    end,
 })
