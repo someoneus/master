d77283e3ca8929725a6646842c20977c5f7947c0
someoneus <wilsonjunor7@gmail.com>
2025-03-06 20:18:35 +0700

    Update tabhub.lua

--- tabhub.lua
+++ tabhub.lua
@@ -6,12 +6,19 @@
 
 
 
+local letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+local randomLetter
+for i = 1, 10 do
+    local randomIndex = math.random(1, #letters)
+    local randomLetter = letters:sub(randomIndex, randomIndex)
+    randomLetter = randomLetter
+end
 
 
 local a
 a = 0
 
-
+local player = game:GetService("Players")
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Tab Hub",
@@ -35,16 +42,19 @@ local Window = Rayfield:CreateWindow({
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
 
-   KeySystem = false, -- Set this to true to use our key system
+		--ignore this key system i just wanna troll my freind
+if player.UserID ~= 7668463473 and randomLetter then
+   KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      Title = "apalu",
+      Subtitle = "key nya "..randomLetter,
+      Note = "harus sama salah dikit ga bisa yahaha", -- Use this to tell the user how to get a key
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {randomLetter} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
+end
 })
 
 local Tab = Window:CreateTab("Scripts", "scroll-text")
