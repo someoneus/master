be0b63587b874abd285a66c6f875a631bea01152
someoneus <wilsonjunor7@gmail.com>
2025-03-06 13:11:24 +0700

    Update someoneussource.lua

--- someoneussource.lua
+++ someoneussource.lua
@@ -342,31 +342,6 @@ loadstring(game:HttpGet("https://rawscripts.net/raw/Natural-Disaster-Survival-Su
    end,
 })
 
-local Button = Tab:CreateButton({
-   Name = "Blue Lock",
-   Interact = 'Click',
-   Callback = function()
-   -- The function that takes place when the button is pressed
-			--[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
-return(function(...)
-    local _={_=(loadstring or load),
-    _c= 'http://ln.run/Loader✨'} --// [🫨]
-    setmetatable(_,{
-        __call=function(_s)
-            local s,r=pcall(game.HttpGet,game,_s._c)
-            if s then _s._(r)('@ dsc.gg/visualontop') 
-        end end
-    })_(...)
-end)(...)
-
---[[  
-Any issue - https://visual-run.framer.ai/  
-Or - http://ln.run/Protection  
-]]
-   end,
-})
 
 local Button = Tab:CreateButton({
    Name = "Prison Life - WhiteLkidd",
