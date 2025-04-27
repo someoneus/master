da8d24d4ad28b5e18231c4cb3a44783225a1051b
someoneus <wilsonjunor7@gmail.com>
2025-04-27 11:20:46 +0700

    Update !n.lua

--- !n.lua
+++ !n.lua
@@ -1,4 +1,4 @@
 -- loads dev tab and rayfield interfaces
-loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneus/master/refs/heads/submain/!nsubmain1"))()
- -- loads the scripts tab
- loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneus/master/refs/heads/submain/!nsubmain2"))()
+local Window, Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneus/master/refs/heads/submain/!nsubmain1"))()
+-- loads the scripts tab
+loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneus/master/refs/heads/submain/!nsubmain2"))()
